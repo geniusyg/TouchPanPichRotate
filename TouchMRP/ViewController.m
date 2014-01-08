762f727e21cc679b24875aebf6a2d710e1b5e4df
Chae Yungi <l8673901@gmail.com>
2014-01-09 01:29:18 +0900

    TouchPanPichRotate
    TouchPanPichRotate

--- TouchMRP/ViewController.m
+++ TouchMRP/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 
 @end
 
@@ -18,6 +19,19 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePan:)];
+    
+    [self.image addGestureRecognizer:pan];
+    
+    UIPinchGestureRecognizer *pinch = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handlePinch:)];
+    
+    [self.image addGestureRecognizer:pinch];
+    
+    UIRotationGestureRecognizer *rotate = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(handleRotate:)];
+    
+    [self.image addGestureRecognizer:rotate];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +39,19 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)handleRotate:(UIRotationGestureRecognizer *)gesture {
+    self.image.transform = CGAffineTransformRotate(self.image.transform, gesture.rotation);
+}
+- (void)handlePinch:(UIPinchGestureRecognizer *)gesture {
+    CGFloat scale = gesture.scale;
+    self.image.transform = CGAffineTransformScale(self.image.transform, scale, scale);
+    
+    [gesture setScale:1.0];
+}
+- (IBAction)handlePan:(UIPanGestureRecognizer *)gesture {
+    CGPoint point = [gesture locationInView:self.view];
+    self.image.center = point;
+}
+
 
 @end
